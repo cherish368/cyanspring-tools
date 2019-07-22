1e74eef7dffb28412e4e82e868c3541db8cad47f
cherish368 <546241694@qq.com>
2019-07-22 17:31:33 +0800

    修复K线数据修正

--- src/main/java/org/cyanspring/tools/common/Attribute.java
+++
@@ -1,9 +0,0 @@
-package org.cyanspring.tools.common;
-
-import java.lang.annotation.*;
-
-@Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD})
-public @interface Attribute {
-}
--- src/main/java/org/cyanspring/tools/utils/PropertiesUtil.java
+++ src/main/java/org/cyanspring/tools/utils/PropertiesUtil.java
@@ -1,14 +1,11 @@
 package org.cyanspring.tools.utils;
 
-import org.cyanspring.tools.common.Attribute;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 
-import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.util.Properties;
 import java.util.ResourceBundle;
@@ -17,39 +14,6 @@ public class PropertiesUtil {
 
     private static final Logger log = LoggerFactory.getLogger(PropertiesUtil.class);
     private static final Properties prop = new Properties();
-
-    /**
-     * 根据配置文件对实体固定属性赋值
-     * 获取配置文件实体
-     *
-     * @param configName
-     * @param t
-     * @param <T>
-     * @return
-     */
-    public static <T> T getProperties(String path, String configName, T t) {
-        ResourceBundle resource = ResourceBundle.getBundle(path + configName);
-        try {
-            Class clazz = t.getClass();
-            Field[] fields = clazz.getDeclaredFields();
-            for (Field field : fields) {
-                field.setAccessible(true);
-                boolean flag = field.isAnnotationPresent(Attribute.class);
-                if (flag) {
-                    String name = field.getName();
-                    String string = resource.getString(configName + "." + name);
-                    Object obj = field.get(t);
-                    if (!StringUtils.isEmpty(string) && obj == null) {
-                        log.info("PropertiesUtil field {}", string);
-                        field.set(t, string);
-                    }
-                }
-            }
-        } catch (IllegalAccessException e) {
-            log.error(e.getMessage());
-        }
-        return t;
-    }
 
 
     public static String getAttribute(String configName, String key) {
